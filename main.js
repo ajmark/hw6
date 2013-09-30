2b2817002ecddae059fdf7164286523eb0c9c47c
Alex Mark <ajmark@andrew.cmu.edu>
2013-09-30 15:30:49 -0400

    hosted jquery files for backlift

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function rateCereal(){
+function rateCereal(number){
   var rating = {0:"I hate this cereal!",
                 25:"This cereal is kind of gross...",
                 50:"I'll eat this cereal.",
                 75:"This cereal is pretty good.",
                 100:"OMGILOVETHISCEREAL!!!!!!"
-  }
+  };
   
-  document.getElementById("response").innerHTML = "<h4>"+rating[document.getElementById("slider-step").value]+"</h4>";
+  document.getElementById('response'+number).innerHTML = "<h4>"+rating[document.getElementById("slider-step"+number).value]+"</h4>";
   
 };
