ebebf08f173440bee9283f875ac483c6494a968b
Alex Mark <ajmark@andrew.cmu.edu>
2013-09-29 23:02:36 -0400

    slider and rater

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-function displayPicture(){
-    var cereal = document.getElementById("cheerios").getAttribute("id");
-    $("#cheerios").append("<img src='images/"+cereal+".jpg'>");
+function rateCereal(){
+  var rating = {0:"I hate this cereal!",
+                25:"This cereal is kind of gross...",
+                50:"I'll eat this cereal.",
+                75:"This cereal is pretty good.",
+                100:"OMGILOVETHISCEREAL!!!!!!"
+  }
+  
+  document.getElementById("response").innerHTML = "<h4>"+rating[document.getElementById("slider-step").value]+"</h4>";
+  
 };
